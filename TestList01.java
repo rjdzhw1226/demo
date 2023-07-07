37a924000f8613cab2181bc19831fe855ec93565
任嘉冬 <11606722+ren-jiadong@user.noreply.gitee.com>
2023-07-07 10:48:07 +0800

    全排列

--- TestList01.java
+++ TestList01.java
@@ -230,4 +230,40 @@ public class TestList01 {
     index = index + 1;
     findStr(haystack, needle, index, list);
   }
+
+  //全排列
+  private List<List<String>> extracted(String[] words, List<String> list) {
+    List<String> temp = new ArrayList<>(list);
+    if (words.length == list.size()){
+      res.add(list);
+    }
+    for (int i = 0; i < words.length; i++) {
+      if(temp.contains(words[i])){
+        continue;
+      }
+      temp.add(words[i]);
+      extracted(words,temp);
+      temp.remove(temp.size() - 1);
+    }
+
+    return res;
+  }
+
+  public static void perm(int[] array, Stack<Integer> stack) {
+    if(array.length <= 0) {
+      //进入了叶子节点，输出栈中内容
+      System.out.println(stack);
+    } else {
+      for (int i = 0; i < array.length; i++) {
+        //tmepArray是一个临时数组，用于就是Ri
+        //eg：1，2，3的全排列，先取出1，那么这时tempArray中就是2，3
+        int[] tempArray = new int[array.length-1];
+        System.arraycopy(array,0,tempArray,0,i);
+        System.arraycopy(array,i+1,tempArray,i,array.length-i-1);
+        stack.push(array[i]);
+        perm(tempArray,stack);
+        stack.pop();
+      }
+    }
+  }
 }
